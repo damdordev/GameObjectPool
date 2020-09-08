d5525d4e18b259259e51948bfe24d7a411b3ae25
Damian Doroba <damian.doroba@gmail.com>
2020-09-08 23:32:10 +0200

    little structure

---
+++ Assets/Damdor/Code/GameObjectPool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Damdor
+{
+    public class GameObjectPool : MonoBehaviour
+    {
+        public UnityEvent<GameObject> onCreate = new UnityEvent<GameObject>();
+        public UnityEvent<GameObject> onPop = new UnityEvent<GameObject>();
+        public UnityEvent<GameObject> onPush = new UnityEvent<GameObject>();
+
+        public GameObject Pop(GameObject prefab) => null;
+        public T Pop<T>(T prefab) where T : Component => null;
+        public T Pop<T>(GameObject prefab) where T : Component => null;
+        public void Push(GameObject gameObjectToPush) {}
+
+        public void Clear() { }
+        public void Clear(GameObject prefab) { }
+    }
+}
---
+++ Assets/Damdor/Tests/GameObjectPoolTest.cs
@@ -0,0 +1,32 @@
+﻿using Damdor;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests.Damdor
+{
+    public class GameObjectPoolTest
+    {
+        private GameObjectPool pool;
+        private GameObject prefab1;
+        private GameObject prefab2;
+
+        [SetUp]
+        public void Setup()
+        {
+            var gameObject = new GameObject();
+            pool = gameObject.AddComponent<GameObjectPool>();
+         
+            prefab1 = new GameObject("prefab1");
+            prefab2 = new GameObject("prefab2");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            GameObject.Destroy(pool.gameObject);
+            GameObject.Destroy(prefab1);
+            GameObject.Destroy(prefab2);
+        }
+        
+    }
+}
